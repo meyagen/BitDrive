92e69b42b68ba02a992c2d8c9416055f1f1c2ffe
Neil Calabroso <nmcalabroso@up.edu.ph>
2015-05-25 02:26:10 +0800

    Automatically compiles using gcc-4.9; Show all warnings

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-gcc client.c -o client
+gcc-4.9 client.c -o client -Wall
 echo "Client compilation completed!"
-gcc server.c -o server -lpthread
+gcc-4.9 server.c -o server -lpthread -Wall
 echo "Server compilation completed!"
 echo "Compilation completed!"
